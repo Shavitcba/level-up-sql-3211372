da71078f00108cff494fc691914779d819a908a5
Shavit <95767833+Shavitcba@users.noreply.github.com>
2023-06-14 15:20:29 +0000

    done

--- 01_restaurant/challenges/01_04_newcustomer_ch.sql
+++ 01_restaurant/challenges/01_04_newcustomer_ch.sql
@@ -4,3 +4,14 @@
 -- Anna Smith (asmith@samoca.org)
 -- 479 Lapis Dr., Memphis, TN
 -- Phone: (555) 555-1212; Birthday: July 21, 1973
+
+INSERT INTO Customers 
+(FirstName, Lastname, Email, Address, City, State, Phone, Birthday)
+VALUES('Anna', 'Smith', 'asmith@samoca.org', '479 Lapis Dr.', 'Memphis', 'TN', '555-555-1212', 'July 21, 1973');
+
+
+--DELETE FROM Customers WHERE CustomerID =102
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
+
+
