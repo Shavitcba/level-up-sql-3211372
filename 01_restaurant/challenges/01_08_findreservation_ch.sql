ebdbe1c7f3d07f347a70149829c82e758f0ea486
Shavit <95767833+Shavitcba@users.noreply.github.com>
2023-06-17 14:44:41 +0000

    done

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -3,5 +3,12 @@
 
 -- Variations of the name include:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
+SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+FROM Reservations
+JOIN customers 
+ON Customers.CustomerID = Reservations.CustomerID
+WHERE Customers.LastName LIKE '%st%' 
+AND Reservations.PartySize=4
+ORDER BY Reservations.Date DESC;
 
 -- There are four people in the party. Today is June 14th.
